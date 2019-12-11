017a0c8f1064c4ee77d220fd6d79904e84069a7c
Dallas James <dallasjames42@gmail.com>
2019-12-10 19:02:06 -0700

    users is done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code away!
+const express = require("express")
+const server =  express()
+const users = require("./users/userRouter")
+const logger = require("./middleware/logger")
+
+server.use(express.json())
+
+server.use(logger())
+server.use("/api/users", users)
+
+server.listen(4000, () => {
+    console.log("\n*** Server Running on http://localhost:4000 ***\n")
+})
