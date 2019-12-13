1678ed6c79075fd660dd442ea7794a0e912f9bdc
Dallas James <dallasjames42@gmail.com>
2019-12-12 18:14:23 -0700

    asdf

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ const server =  express()
 const users = require("./users/userRouter")
 const posts = require("./posts/postRouter")
 const logger = require("./middleware/logger")
-// const host = process.env.HOST || "127.0.0.1"
-// let port = process.env.PORT || 8080
+const host = process.env.HOST || "127.0.0.1"
+let port = process.env.PORT || 8080
 
 dotenv.config()
 
 server.use(express.json())
 
 server.use(logger())
-server.use("/", (req,res) => {
-    res.status(200).json({
-        message: "you made it"
-    })
-})
+// server.use("/", (req,res) => {
+//     res.status(200).json({
+//         message: "you made it"
+//     })
+// })
 server.use("/api/users", users)
 server.use("/api/posts", posts)
 
-server.listen(process.env.PORT || 3000, () => {
+server.listen(port, host, () => {
     console.log(`\n*** Server Running on whatever it wants ***\n`)
 })
